e1fe51e43b68613a3feda66934eae3c6e2077cf3
Ruslan Rudnev <r.rudnev@gmail.com>
2017-11-26 20:06:33 -0500

    extern c called correct

---
+++ foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+void foo(void)
+{
+    printf("hi from c");
+}
--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 
 using namespace std;
 
-int main() {
-    string aa = "Hello";
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << aa << std::endl;
-    Customer customer = Customer("John", "Smith");
-    std::cout << customer.name << std::endl;
-    std::cout << customer.age << std::endl;
 
+extern "C" {
+#include "foo.h" //a C header, so wrap it in extern "C"
+}
+
+int main() {
+//    string aa = "Hello";
+//    std::cout << "Hello, World!" << std::endl;
+//    std::cout << aa << std::endl;
+//    Customer customer = Customer("John", "Smith");
+//    std::cout << customer.name << std::endl;
+//    std::cout << customer.age << std::endl;
+    foo();
     return 0;
 }

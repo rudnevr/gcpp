f1f36d3e9eadb5663ac54c8034fb901cd23f7d2e
Ruslan Rudnev <r.rudnev@gmail.com>
2017-11-26 22:54:03 -0500

    enumwindows works

--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 #include "Customer.h"
+#include <windows.h>
+#include <string>
+#include <iostream>
 
 using namespace std;
-
 
 extern "C" {
 #include "foo.h" //a C header, so wrap it in extern "C"
 }
 
+BOOL CALLBACK enumWindowsProc(
+        HWND hWnd,
+        LPARAM lParam
+) {
+//    if( !::IsIconic( hWnd ) ) {
+//        return TRUE;
+//    }
+
+    int length = ::GetWindowTextLength(hWnd);
+    if (0 == length) return TRUE;
+    LPTSTR lptstr = (LPTSTR) lParam;
+    char cWindow[length];
+//    HWND wnd = GetForegroundWindow();
+    GetWindowTextA(hWnd, cWindow, length);
+//    printf("lparam %s\r\n", lptstr);
+//    printf("cWindows %s\r\n", cWindow);
+    const basic_string<char> &sout = string(cWindow);
+    if (sout.find(lptstr) != string::npos) {
+        cout << sout << std::endl;
+        return FALSE;
+    }
+    return TRUE;
+}
+
 int main() {
-//    string aa = "Hello";
-//    std::cout << "Hello, World!" << std::endl;
-//    std::cout << aa << std::endl;
-//    Customer customer = Customer("John", "Smith");
-//    std::cout << customer.name << std::endl;
-//    std::cout << customer.age << std::endl;
+    BOOL enumeratingWindowsSucceeded = ::EnumWindows(enumWindowsProc, (LPARAM) (LPTSTR) "CLion");
     foo();
     return 0;
 }

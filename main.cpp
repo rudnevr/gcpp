02e7b090848ef78b4408149cf8aef5b7e85b5444
Ruslan Rudnev <r.rudnev@gmail.com>
2017-11-27 00:34:38 -0500

    regex worksi

--- main.cpp
+++ main.cpp
@@ -3,39 +3,45 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+#include <regex>
+#include <list>
 
 using namespace std;
 
 extern "C" {
 #include "foo.h" //a C header, so wrap it in extern "C"
 }
-
+std::list<HWND> list;
+HWND current;
 BOOL CALLBACK enumWindowsProc(
         HWND hWnd,
         LPARAM lParam
 ) {
-//    if( !::IsIconic( hWnd ) ) {
-//        return TRUE;
-//    }
-
     int length = ::GetWindowTextLength(hWnd);
     if (0 == length) return TRUE;
-    LPTSTR lptstr = (LPTSTR) lParam;
+    regex *regex1 = reinterpret_cast<std::regex *>(lParam);
     char cWindow[length];
-//    HWND wnd = GetForegroundWindow();
     GetWindowTextA(hWnd, cWindow, length);
-//    printf("lparam %s\r\n", lptstr);
+//    printf("lparam %s\r\n", regex1);
 //    printf("cWindows %s\r\n", cWindow);
     const basic_string<char> &sout = string(cWindow);
-    if (sout.find(lptstr) != string::npos) {
+    if (std::regex_match(sout, *regex1)) {
         cout << sout << std::endl;
-        return FALSE;
+        if (hWnd != current) {
+            cout << "match!" << std::endl;
+            current=hWnd;
+            return FALSE;
+        }
+        return TRUE;
     }
     return TRUE;
 }
 
 int main() {
-    BOOL enumeratingWindowsSucceeded = ::EnumWindows(enumWindowsProc, (LPARAM) (LPTSTR) "CLion");
+    current = GetForegroundWindow();
+    std::regex txt_regex(".*(CLion|IntelliJ|cmd|Total).*");
+    EnumWindows(enumWindowsProc, reinterpret_cast<LPARAM >(&txt_regex));
+    EnumWindows(enumWindowsProc, reinterpret_cast<LPARAM >(&txt_regex));
     foo();
     return 0;
 }
